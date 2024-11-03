4590a190bef6ccced2321ac398c828b4d09d280f
anjalii14 <anjjalisingh14@gmail.com>
2024-11-03 22:29:35 +0530

    added docker

--- Dockerfile
+++ Dockerfile
@@ -16,14 +16,13 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy the rest of the application code into the container
 COPY src /app/src
 
-# Copy the .env file into the container (replace with your Mongo URI as needed)
-COPY .env /app/.env
-
-# Expose the port that the FastAPI app runs on
 EXPOSE 8000
 
-# Set environment variables for MongoDB URI
-ENV MONGO_URI=your_mongo_uri_here
+ENV MONGO_URI=""
+
+# Copy the .env file into the container
+COPY .env /app/.env
+
 
 # Run the FastAPI application using Uvicorn
 CMD ["uvicorn", "src.app:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- src/app.py
+++ src/app.py
@@ -2,28 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routes.graph_crud_routes import router as graph_crud_router
 from src.routes.graph_run_routes import router as graph_run_router
-
-app = FastAPI(title="Graph Processing API", version="1.0")
-
-# Set up CORS Middleware to allow cross-origin requests from the frontend
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
+
+app = FastAPI(title="Graph Processing API", version="1.0")
 
 # Load environment variables from .env file
 load_dotenv()
